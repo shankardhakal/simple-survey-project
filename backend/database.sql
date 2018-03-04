19777649ec7f3dca9fa7476472a08795d3a2443c
Shankar Dhakal <info.shandhakal@gmail.com>
2018-03-04 19:32:59 +0200

    files moved to backend

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `id2678951_zhanglion:` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `id2678951_zhanglion`;
 -- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: survey
+-- Host: localhost    Database: id2678951_zhanglion:
 -- ------------------------------------------------------
 -- Server version	5.7.21-0ubuntu0.17.10.1
 
@@ -36,7 +38,7 @@ CREATE TABLE `options` (
 
 LOCK TABLES `options` WRITE;
 /*!40000 ALTER TABLE `options` DISABLE KEYS */;
-INSERT INTO `options` (`id`, `option_name`) VALUES (1,'Yes'),(2,'No'),(3,'Maybe');
+INSERT INTO `options` VALUES (1,'Yes'),(2,'No'),(3,'Maybe');
 /*!40000 ALTER TABLE `options` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,7 +53,8 @@ CREATE TABLE `questions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime DEFAULT CURRENT_TIMESTAMP,
   `question` varchar(1000) DEFAULT '',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `questions_id_uindex` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -61,7 +64,7 @@ CREATE TABLE `questions` (
 
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
-INSERT INTO `questions` (`id`, `created`, `question`) VALUES (1,'2018-03-04 10:40:10','Volunteer for a cause'),(2,'2018-03-04 10:40:10','Declutter and donate'),(3,'2018-03-04 10:40:10','Decrease your use of plastic'),(4,'2018-03-04 10:40:10','Adopt a rescue pet'),(5,'2018-03-04 10:40:10','Focus on the good'),(6,'2018-03-04 10:40:10','Travel respectfully');
+INSERT INTO `questions` VALUES (1,'2018-03-04 10:40:10','Volunteer for a cause'),(2,'2018-03-04 10:40:10','Declutter and donate'),(3,'2018-03-04 10:40:10','Decrease your use of plastic'),(4,'2018-03-04 10:40:10','Adopt a rescue pet'),(5,'2018-03-04 10:40:10','Focus on the good'),(6,'2018-03-04 10:40:10','Travel respectfully');
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,10 +79,13 @@ CREATE TABLE `results` (
   `question_id` int(11) DEFAULT NULL,
   `option_id` int(11) DEFAULT NULL,
   `created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) DEFAULT NULL,
   KEY `option_id` (`option_id`),
   KEY `question_id` (`question_id`),
+  KEY `user_id` (`user_id`),
   CONSTRAINT `option_id` FOREIGN KEY (`option_id`) REFERENCES `options` (`id`),
-  CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
+  CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -89,7 +95,34 @@ CREATE TABLE `results` (
 
 LOCK TABLES `results` WRITE;
 /*!40000 ALTER TABLE `results` DISABLE KEYS */;
+INSERT INTO `results` VALUES (2,1,'2018-03-04 11:44:46',1),(1,1,'2018-03-04 15:18:11',1),(2,3,'2018-03-04 15:18:11',1),(3,2,'2018-03-04 15:18:11',1),(1,1,'2018-03-04 15:22:47',5),(2,3,'2018-03-04 15:22:47',5),(3,2,'2018-03-04 15:22:47',5);
 /*!40000 ALTER TABLE `results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_identifier` varchar(42) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_id_uindex` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'2018-03-04 11:32:07','9762ec443a4cb446f0365792643ade400e4cad67'),(2,'2018-03-04 11:45:38','1f1ab7a75ea55a13f35614d1c92e0eb1c58b127c'),(3,'2018-03-04 14:41:37','b0c4d00bf13d0a1897038322735b939abc7783ac'),(4,'2018-03-04 14:42:09','4a0d10ee63ecdacb142a91b83ccf139f3ea7a50c'),(5,'2018-03-04 15:22:22','7de722f5063662525d5e2634dedaaae52dbb0c23');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -101,4 +134,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-04 10:44:08
+-- Dump completed on 2018-03-04 15:44:07
---
+++ backend/index.php
@@ -0,0 +1,104 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Medoo\Medoo;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = new \Slim\App;
+
+
+$container = $app->getContainer();
+
+$container['database'] = function () {
+    return new Medoo(
+        [
+            'database_type' => 'mysql',
+            'database_name' => 'sdhakal_survey',
+            'server' => 'localhost',
+            'username' => 'sdhakal_survey',
+            'password' => 'SUPER+500'
+        ]
+    );
+};
+
+$app->get('/questions', function (Request $request, Response $response, array $args) {
+
+    $data = $this->database->select('questions', ['id', 'question']);
+
+    $options = $this->database->select('options', ['id', 'option_name']);
+
+
+    $userIdentifier = sha1(random_bytes(100));
+
+    $this->database->insert(
+        'users',
+        ['user_identifier' => $userIdentifier]
+    );
+
+    $data = json_encode(["user_id" => $this->database->id(), "questions" => $data, "options" => $options]);
+
+    $body = $response->getBody();
+    $body->write($data);
+
+    return $response->withBody($body)
+        ->withHeader('Content-type', 'application/json');
+
+});
+
+$app->post('/response', function (Request $request, Response $response, array $args) {
+
+    $requestStr = $request->getBody()->getContents();
+
+    $userResponse = json_decode($requestStr, true);
+
+$res=    $this->database->insert(
+        'results',
+        $userResponse
+    );
+
+
+
+    $response->getBody()->write(json_encode(['success'=>true]));
+
+    $response->withHeader('Content-type', 'application/json');
+
+    return $response;
+
+});
+
+$app->get('/result', function (Request $request, Response $response, array $args) {
+
+    $count = $this->database->count('users', 'id');
+
+    $response->withHeader('Content-type', 'application/json');
+
+$fetchResultSql = <<<SQL
+SELECT q.question , options.option_name, u.user_identifier
+ from options
+  INNER JOIN results r ON options.id = r.option_id
+  INNER JOIN questions q ON r.question_id = q.id
+  INNER JOIN users u ON r.user_id = u.id 
+  ORDER by user_identifier
+SQL;
+
+    $results = $this->database->query($fetchResultSql)->fetchAll();
+
+ array_walk($results, function (&$array){
+
+    unset($array[0]);
+
+    unset($array[1]);
+
+    unset($array[2]);
+});
+
+
+    $response->getBody()->write(json_encode($results));
+
+    return $response;
+
+});
+
+
+$app->run();
